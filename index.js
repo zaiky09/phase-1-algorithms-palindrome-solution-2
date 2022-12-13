31392bdc749fd12e8e8b092d0a730855cf85192f
zaiky09 <zamilmozamil@outlook.com>
2022-12-13 06:36:22 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+
+    if(word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
